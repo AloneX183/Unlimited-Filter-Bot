4e457f114f65c525a15030cf0f995a7f07ec7686
AloneX <108611845+AloneX183@users.noreply.github.com>
2022-08-31 12:43:20 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -51,8 +51,8 @@ async def cb_handler(client, query):
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/AloneX_Bots"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/AloneXbots_Support")
                 ]
             ]
         )
